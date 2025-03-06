b2c6b2bccbe0fbc2a79e0415743c637261ceb76b
Seu Nome <rahman13@gmail.com>
2025-03-06 17:56:30 +0000

    ajuste

--- App.py
+++ App.py
@@ -22,23 +22,31 @@ def finalizar_app():
     os.system("cls")
     print("Obrigado por usar o nosso app\n")
 
-def escolher_Opcoes():
-    opcao_escolhida = int(input("Escolha uma opção: "))
-    
-    if opcao_escolhida == 1: 
-        print("Cadastrar restaurante")
-    elif opcao_escolhida == 2: 
-        print("Listar restaurantes")
-    elif opcao_escolhida == 3: 
-        print("Ativar restaurante")
-    elif opcao_escolhida == 4:
-        finalizar_app()
-    else: 
-        os.system("cls")
+def opcao_invalida():
         print("Opção inválida, escolha novamente:\n")
-        exibir_menu()
+        input("Pressione Enter para continuar...")
+        main()
+
+
+def escolher_Opcoes():
+    try:
+        opcao_escolhida = int(input("Escolha uma opção: "))
+        
+        if opcao_escolhida == 1: 
+            print("Cadastrar restaurante")
+        elif opcao_escolhida == 2: 
+            print("Listar restaurantes")
+        elif opcao_escolhida == 3: 
+            print("Ativar restaurante")
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else: 
+            opcao_invalida()
+    except:
+        opcao_invalida()
 
 def main():
+    os.system("cls")
     exibir_nome()
     exibir_menu()
 
